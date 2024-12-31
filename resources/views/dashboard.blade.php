41d2b8c50f8dc2b6bb989686dd9d4c2bff7acff7
shohag1610 <shohag.edu96@gmail.com>
2024-12-31 18:07:08 +0000

    Dashboard: design complete

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,90 @@
 @extends('layouts.app')
 
 @section('content')
-    <h5>This is dashboard</h5>
+    <div class="shopping-list">
+        <div class="card">
+            <h5 class="card-title">Shopping Lists <i class="fa-solid fa-rectangle-list"></i></h5>
+
+            {{-- Budget container starts --}}
+            <div class="row mb-3 mt-2">
+                <div class="col">
+                    <label for="budget" class="form-label budget">
+                        Budget: £<span id="budget">{{ Auth::user()->total_budget }}</span>
+                        <a href="javascript:void(0);"
+                            class="d-inline-flex align-items-center text-decoration-none cursor-pointer ms-2">
+                            <i class="fa-solid fa-money-bill-trend-up"></i>
+                            <span class="ms-2">Set Limit</span>
+                        </a>
+                    </label>
+                </div>
+
+                <div class="col" id="updateBudgetContainer" style="display: none">
+                    <div class="input-group">
+                        <input type="number" id="budgetUpdateAmount" class="form-control" placeholder="£ Price">
+                        <button class="btn btn-add">Update</button>
+                    </div>
+                </div>
+
+            </div>
+
+            {{-- Shopping list adder --}}
+            <div class="input-group mb-3">
+                <input type="text" id="itemName" class="form-control" placeholder="Enter item name">
+                <input type="number" id="itemPrice" class="form-control" placeholder="£ Price">
+                <button class="btn btn-add">Add</button>
+            </div>
+
+            <div class="uncheckedItemContainer">
+                <ul class="list-group mb-3" id="itemList">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <div>
+                            <input type="checkbox" class="form-check-input me-2">
+                            <span>Example Item 1</span>
+                        </div>
+                        <div>
+                            £<span class="item-price">10.00</span>
+                            <button class="btn btn-sm btn-danger ms-2">×</button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div id="checkedItemsContainer">
+                <h5 id="checkedItemLabel">Purchased Grocery</h5>
+                <ul class="list-group mb-3" id="checkedItemsList">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <div>
+                            <input type="checkbox" class="form-check-input me-2" checked>
+                            <span style="text-decoration: line-through;">Example Purchased Item</span>
+                        </div>
+                        <div>
+                            £<span class="item-price">5.00</span>
+                            <button class="btn btn-sm btn-danger ms-2">×</button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="d-flex justify-content-between total">
+                <span>Total:</span>
+                <span>£<span id="total">15.00</span></span>
+            </div>
+
+            <div class="row mb-2 mt-3">
+                <div class="col">
+                    <label class="form-label budget">
+                        Share:
+                        <i class="fa-solid fa-envelope"></i>
+                    </label>
+                </div>
+                <div class="col-lg-8" id="sendEmailContainer" style="display: none">
+                    <div class="input-group">
+                        <input type="email" id="emailAddress" class="form-control" placeholder="example@gmail.com">
+                        <button class="btn btn-add">Send</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -42,6 +44,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.min.js"
         integrity="sha512-b+nQTCdtTBIRIbraqNEwsjB6UvL3UEMkXnhzd8awtCYh0Kcsjl9uEgwVFVbhoj3uu1DO1ZMacNvLoyJJiNfcvg=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
